9672dd7e72e684efdc933261b0cfd9cfab4a0da6
mgreis <mgreis@student.dei.uc.pt>
2016-05-25 10:24:27 +0100

    [ADD] algorithm split into functions

--- worker.py
+++ worker.py
@@ -17,39 +17,33 @@ hash[::-1].encode('hex_codec')
 '00000000000000001e8d6829a8a21adc5d38d0a473b144b6765798e61f98bd1d'
 """
 
-def hash_it (c,v):
-    input_string = c+v
-    input_string_bin = unhexlify(input_string)
-    hash = hashlib.sha256(hashlib.sha256(input_string_bin).digest()).digest()
-    return str(hexlify(hash).decode('ascii'))
 
-def gen_target (number_of_zeros):
+def hash_it(chain, value):
+    input_string = chain + value
+    input_string_bin = unhexlify(input_string)
+    result = hashlib.sha256(hashlib.sha256(input_string_bin).digest()).digest()
+    return str(hexlify(result).decode('ascii'))
+
+
+def gen_target(number_of_zeros):
     target = ''.join('0' for n in range(number_of_zeros))
     target = target + ''.join('f' for n in range(64-number_of_zeros))
     return target
 
 
-#def find_proof (c,v):
-
-
-#target_hexstr = "00000f"
-#target =''.join('0' for n in range(5))
-#target = target + ''.join('f' for n in range(59))
-target = gen_target(5)
-print("Target:  "+target)
-
-
-
-
-random_number = ''.join(random.choice('0123456789abcdef') for n in range(64))
-
-#print (target_hexstr)
-#print (random_number)
-
-
-while (str(hash_it(random_number,random_number)) > target):
+def find_proof_of_work(chain,number_of_zeros):
+    target = gen_target(number_of_zeros)
     random_number = ''.join(random.choice('0123456789abcdef') for n in range(64))
-    #print (random_number)
+    while hash_it(chain, random_number)>target:
+        random_number = ''.join(random.choice('0123456789abcdef') for n in range(64))
+    return random_number
 
-print ("Result:  " + hash_it(random_number,random_number))
-print ("Answer:  " + random_number)
+
+chain = ''.join(random.choice('0123456789abcdef') for n in range(64))
+
+answer = find_proof_of_work(chain,5)
+
+print('random: ' + chain)
+print('answer: ' + answer)
+print('hash:   ' + hash_it(chain, answer))
+
